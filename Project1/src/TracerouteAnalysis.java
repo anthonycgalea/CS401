dc250e68f6ed3c8473bb4da4426971cd8e062934
anthonycgalea <44701741+anthonycgalea@users.noreply.github.com>
2020-09-21 19:58:52 -0400

    Update TracerouteAnalysis.java

--- Project1/src/TracerouteAnalysis.java
+++ Project1/src/TracerouteAnalysis.java
@@ -1,9 +1,25 @@
+import java.io.File;
+import java.util.Scanner;
 
 public class TracerouteAnalysis {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Hello! What filename would you like to analyze the connection?");
+		String filePath = null;
+		boolean first = false;
+		File f;
+		do {
+		if (first) {
+			System.out.println("Invalid filename. Please try again. Enter a filename:");
+		}
+		filePath = sc.nextLine();
+		f = new File(filePath);
+		first = true;
+		} while(!f.canRead());
+		sc.close();
+		System.out.println("Valid filename.");
 	}
 
 }
