affdc76748090bdec97fcb5242ed37f1c1df4b39
anthonycgalea <44701741+anthonycgalea@users.noreply.github.com>
2020-10-28 20:12:00 -0400

    d

--- Cw7/src/TurboSearch.java
+++ Cw7/src/TurboSearch.java
@@ -3,9 +3,8 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class TurboSearch {
-    public static void main(String[] args){
-    	ArrayList<Thread> threads = new ArrayList<>();
-    	ArrayList<FileRead> frs = new ArrayList<>();
+    public static void main(String[] args){ 
+    	ArrayList<FileRead> frs = new ArrayList<>(); //for checking if done
         int tally=0; //stores the total number of lines across all files
           if (args.length < 2){
              System.out.println("Usage: java TurboSearch keyword sourcefile1 sourcefile2 . . .");
@@ -17,24 +16,23 @@ public class TurboSearch {
           // your code goes here
           //any additional classes that you add
           //should be added to the same file.
-          FileRead fr = new FileRead(filename,keyword);
-          Thread t = new Thread(fr);
-          threads.add(t);
-          frs.add(fr);
-          t.start(); 
+          FileRead fr = new FileRead(filename,keyword); //create object
+          Thread t = new Thread(fr); //start new thread
+          frs.add(fr); //add object to arrayList
+          t.start(); //run
        }
        while(true) {
-    	   int count = 0;
-    	   for (int i = 0; i < threads.size(); i++) {
+    	   int count = 0; 
+    	   for (int i = 0; i < frs.size(); i++) { //check if all threads are finished running
     		   if (frs.get(i).isDone()) {
     			   count++;
     		   }
     	   }
-    	   if (count == threads.size()-1) {
+    	   if (count == frs.size()-1) {
     		   break;
     	   }
        }
-       for (int i = 0; i < frs.size(); i++) {
+       for (int i = 0; i < frs.size(); i++) { //count up how many results there are
     	   tally+=frs.get(i).getNumLines();
        }
        System.out.println("--------------------------------------------");
@@ -52,7 +50,7 @@ class FileRead implements Runnable {
 	private boolean isDone;
     public FileRead(String fileName, String word) {
     	this.fileName = fileName;
-    	this.word = word.toLowerCase();
+    	this.word = word.toLowerCase(); //case insensitivity
     	this.numLines = 0;
     	this.isDone = false;
     }
@@ -65,19 +63,19 @@ class FileRead implements Runnable {
     	return this.isDone;
     }
     	
-    public void run() {
+    public void run() { 
     	try {
-    		File f = new File(fileName);
+    		File f = new File(fileName); //create file object
     		if (f.canRead()) {
     			@SuppressWarnings("resource")
 				Scanner sc = new Scanner(f);
-    			while(sc.hasNext()) {
+    			while(sc.hasNext()) { //run to end of text file
     				String s = sc.nextLine();
-    				String sLower = s.toLowerCase();
+    				String sLower = s.toLowerCase(); //to make case-insensitive
     				if (sLower.contains(this.word)) {
     					this.numLines++;
     					if (s.length() > 40) {
-    						int index = sLower.indexOf(this.word);
+    						int index = sLower.indexOf(this.word); //for truncation
     						if (index <=70-this.word.length()) {
     							s = s.substring(0,70) + " [..output truncated]";
     						} else {
